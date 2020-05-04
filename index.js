b4800501f73ee2afd87877ebf2338427d604bac6
Gilles Piou <gilles.piou@gmail.com>
2020-05-04 18:55:35 +0800

    Support macos and windows environments

--- index.js
+++ index.js
@@ -2,24 +2,29 @@
 
 const { promisify } = require("util");
 const exec = promisify(require("child_process").exec);
+const fs = require("fs");
+const os = require("os");
+const path = require("path");
 const core = require("@actions/core");
 
 async function run() {
   try {
+    const homeDirectory = os.homedir();
+    const sshDirectory = path.join(homeDirectory, ".ssh");
     const GH_SSH_KEY = core.getInput("GH_SSH_KEY", { required: true });
-    [
-      "mkdir -p ~/.ssh",
-      "chmod 700 ~/.ssh",
-      "touch ~/.ssh/known_hosts",
-      "ssh-keyscan github.com >> ~/.ssh/known_hosts",
-      "chmod 600 ~/.ssh/known_hosts",
-      `echo "${GH_SSH_KEY}" > ~/.ssh/id_rsa`,
-      "chmod 600 ~/.ssh/id_rsa",
-      "eval $(ssh-agent -s) && ssh-add ~/.ssh/id_rsa"
-    ].reduce(async (acc, cmd) => {
-      await acc;
-      return exec(cmd);
-    }, Promise.resolve({}));
+    fs.mkdirSync(sshDirectory, { recursive: true, mode: 0o700 });
+
+    const id_rsa = path.join(sshDirectory, "id_rsa");
+    fs.writeFileSync(id_rsa, `${GH_SSH_KEY}\n`, { mode: 0o600 });
+    if (process.platform === "darwin") {
+      await exec("ssh-keyscan -H github.com > ~/.ssh/known_hosts");
+    }
+    const cmd =
+      {
+        darwin: "eval $(ssh-agent -s) && ssh-add -K ~/.ssh/id_rsa",
+        win32: ":",
+      }[process.platform] || "eval $(ssh-agent -s) && ssh-add ~/.ssh/id_rsa";
+    await exec(cmd);
   } catch (error) {
     core.setFailed(error.message);
   }
